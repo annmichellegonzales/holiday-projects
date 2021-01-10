79206190a168c6238714af785ddd42b2389f2781
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-01-10 17:23:42 -0600

    practice js 71/101 problems

---
+++ js/Rudy.js
@@ -0,0 +1,6 @@
+var howOld = prompt("How old are you?")
+
+function cool() {
+    return "Wow you are " + howOld + ". That's cool"
+}
+console.log(cool);
---
+++ js/loopsnmore.js
@@ -0,0 +1,37 @@
+"use strict";
+
+console.log("test");
+
+
+
+// Exercise 71
+// Write a function definition named hasEvens that takes in sequence of numbers and returns true if there are any even numbers in the sequence
+//
+// function hasEvens(x) {
+//     var anArray = [];
+//     return x.filter(n === n%2);
+// }
+
+// function hasEvens(x) {
+//     var arr = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         return arr[i] % 2 === 0;
+//     }
+// }
+
+
+// function getEvenNumbers() {
+//     var arr = [1, 2, 3, 4, 5, 6];
+//
+//     for (var i = 0; i < arr.length; i++) {
+//         if (i % 2 === 0) {
+//             return true;
+//         }
+//     }
+// }
+
+// assert(hasEvens([1, 2, 3]), true, "Exercise 71");
+// assert(hasEvens([2, 5, 6]), true, "Exercise 71");
+// assert(hasEvens([3, 3, 3]), false, "Exercise 71");
+// assert(hasEvens([]), false, "Exercise 71");
+// addToDone("Exercise 71 is correct.");
